ced1e98dbcb90ae65e7c07e07e7793f5157d5a20
msharaf315 <msharaf315@gmail.com>
2019-12-05 19:26:47 +0300

    added comments

--- client/App.js
+++ client/App.js
@@ -1,21 +1,18 @@
-
-
-
-
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import home from './components/home' 
-
+import { createAppContainer } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
+import home from "./components/home"
+require("dotenv").config()
+console.log(process.env)
 const MainNavigator = createStackNavigator({
   Home: {
     screen: home,
     navigationOptions: {
-        header: null,
-        gesturesEnabled: false,
-    },
-},
-});
+      header: null,
+      gesturesEnabled: false
+    }
+  }
+})
 
-const App = createAppContainer(MainNavigator);
+const App = createAppContainer(MainNavigator)
 
-export default App;
+export default App
--- routes/chatBot.js
+++ routes/chatBot.js
@@ -1,41 +1,35 @@
-const AssistantV1 = require('ibm-watson/assistant/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
+const AssistantV1 = require("ibm-watson/assistant/v1")
+const { IamAuthenticator } = require("ibm-watson/auth")
 
-
-
-const express = require('express');
-const router = express.Router();
-const service = new AssistantV1({ 
-  version: '2019-02-28',
+const express = require("express")
+const router = express.Router()
+const service = new AssistantV1({
+  version: "2019-02-28",
+  // sheyl el url dah 7oto fe .env file
+  // ba3dyn e3ml hena process.env.apikey
+  // we  process.env.url
+  // supposedly haysht8al
   authenticator: new IamAuthenticator({
-    apikey: 'E3VBZU1rwsJiJBCYrNsaGb6oNZ4IznmXvntkBdrFB90y',
+    apikey: "E3VBZU1rwsJiJBCYrNsaGb6oNZ4IznmXvntkBdrFB90y"
   }),
-  url: 'https://gateway-lon.watsonplatform.net/assistant/api'
-});
- 
-router.post('/', async (req, res) => {
-    console.log(req.body.input)
-    await service.message({
-        workspaceId: '44bb09f9-58bf-49bb-be95-7bf07e829aee',
-      input: {'text':req.body.input},
+  url: "https://gateway-lon.watsonplatform.net/assistant/api"
+})
 
-      })
+router.post("/", async (req, res) => {
+  console.log(req.body.input)
+  await service
+    .message({
+      workspaceId: "44bb09f9-58bf-49bb-be95-7bf07e829aee",
+      input: { text: req.body.input }
+    })
 
-      .then(resp=>{
-          res.json({data:  resp.result.output.text[0]})
-          console.log(resp.result.output.text[0])
-      }
+    .then(resp => {
+      res.json({ data: resp.result.output.text[0] })
+      console.log(resp.result.output.text[0])
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
-     )
-      .catch(err => {
-        console.log(err);
-
-
-      });
-
-    }
-);
-
-
-
-  module.exports = router 
+module.exports = router
